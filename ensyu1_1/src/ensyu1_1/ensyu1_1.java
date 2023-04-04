5b2d2dc4405aa52cfa16d518b6861f1f9491e9a3
ddt-akamine <a@abc.com>
2023-04-04 18:08:21 +0900

    関数ヘッダおよびコメントを追加しました。

--- ensyu1_1/src/ensyu1_1/ensyu1_1.java
+++ ensyu1_1/src/ensyu1_1/ensyu1_1.java
@@ -1,8 +1,18 @@
 package ensyu1_1;
 
 public class ensyu1_1 {
+	/**
+	 * @関数名: main
+	 * @メソッドの説明 : セミコロンがない状態でコンパイルするとエラーがでる
+	 * @パラメータの説明 : なし
+	 * @返り値 : なし
+	 * @作成者 : 赤嶺亮介
+	 * @日付 : 2023/04/04
+	 */
 	public static void main(String[] arg){
+		//エラー該当箇所
 		System.out.println("Hello World !!")
+		//エラーが無い場所
 		System.out.println("Hello World !!");
 	}
 }
