af415c78c4841d8aa5c52f897fd3b90e59559933
ddt-akamine <a@abc.com>
2023-04-05 16:53:11 +0900

    修正：クラスヘッダを追加・コメント分を変更

--- ensyu1_3/src/ensyu1_3/ensyu1_3.java
+++ ensyu1_3/src/ensyu1_3/ensyu1_3.java
@@ -1,5 +1,10 @@
 package ensyu1_3;
-
+/*
+ * クラス名:ensyu1_3
+ * 概要  :演習1-3クラス
+ * 作成者:R.Aakamine
+ * 作成日:2023.04.05
+ */
 public class ensyu1_3 {
 	/**
 	 * @関数名: main
@@ -10,8 +15,7 @@ public class ensyu1_3 {
 	 * @日付 : 2023/04/05
 	 */
 	public static void main(String[] args) {
-		//改行コード\nを使用して表示
-		//姓と名の間に空白を作るために"\n"を挿入
+		//苗字と名前の間に1マス開けて縦表示
 		System.out.println("赤\n嶺\n" + "\n" + "亮\n介");
 	}
 }
