558f5219004dfe00f98d25ac4e73413adc4ae029
ddt-akamine <a@abc.com>
2023-04-05 10:27:11 +0900

    修正：関数ヘッダを追加

--- ensyu1_2/src/ensyu1_2/ensyu1_2.java
+++ ensyu1_2/src/ensyu1_2/ensyu1_2.java
@@ -1,7 +1,16 @@
 package ensyu1_2;
 
 public class ensyu1_2 {
+	/**
+	 * @関数名: main
+	 * @メソッドの説明 : 自分の名前を表示
+	 * @パラメータの説明 : なし
+	 * @返り値 : なし
+	 * @作成者 : 赤嶺亮介
+	 * @日付 : 2023/04/04
+	 */
 	public static void main(String[] arg) {
+		//文字列の中に改行コード\nを入れ、表示
 		System.out.println("赤\n嶺\n亮\n介");
 	}
 }
