c3ffd257a09f1e3c66714baaf14a089470f5c245
ddt-akamine <a@abc.com>
2023-04-07 10:33:11 +0900

    修正：stdIn →　standardInput

--- ensyu2_6/src/ensyu2_6/ensyu2_6.java
+++ ensyu2_6/src/ensyu2_6/ensyu2_6.java
@@ -18,17 +18,17 @@ public class ensyu2_6 {
 	 */
 	public static void main(String[] args) {
 		//キーボードから入力した値を格納するための変数を用意
-		Scanner stdIn = new Scanner(System.in);
+		Scanner standardInput = new Scanner(System.in);
 		//プログラム概要を表示
 		System.out.println("三角形の面積を求めます。");
 		//底辺の値を入力するように促す
 		System.out.print("底辺：");
 		//底辺の値を計算できるようにデータを移動
-		double triangleBase = stdIn.nextDouble();
+		double triangleBase = standardInput.nextDouble();
 		//高さの値を入力するように促す
 		System.out.print("高さ：");
 		//高さの値を計算できるようにデータを移動
-		double triangleHeight = stdIn.nextDouble();
+		double triangleHeight = standardInput.nextDouble();
 		//三角形の面積を計算して、表示する
 		System.out.println("三角形の面積は" + triangleBase*triangleHeight/2 + "です。");
 	}
