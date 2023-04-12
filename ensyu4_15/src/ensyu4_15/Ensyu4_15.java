e22125b64ccba64097c2b424cdb6b12f47fd4bd3
ddt-akamine <a@abc.com>
2023-04-12 11:45:04 +0900

    aaa

--- ensyu4_15/src/ensyu4_15/Ensyu4_15.java
+++ ensyu4_15/src/ensyu4_15/Ensyu4_15.java
@@ -20,32 +20,32 @@ public class Ensyu4_15 {
 		//標準入力のための準備
 		Scanner standardInput = new Scanner(System.in) ;
 		//開始値を保存
-		int startValue;
+		int startValue=0;
 		//最終値を保存
-		int finalValue;
+		int finalValue=0;
 		//増分を保存
-		int differenceValue;
+		int differenceValue=0;
 		do {
 			//開始の身長を入力
-			System.out.println("開始：何ｃｍから：");
+			System.out.print("開始何ｃｍから：");
 			//計算できるように処理
 			startValue = standardInput.nextInt();
 		//負の値が入力されると再度入力
 		}while(startValue<0);
 		do {
 			//終了値を入力
-			System.out.println("終了：何ｃｍまで：");
+			System.out.print("終了何ｃｍまで：");
 			//計算できるように処理
 			finalValue = standardInput.nextInt();
 		//負の値が入力されると再度入力
 		}while(finalValue<0);
 		do {
 			//増分を入力
-			System.out.println("増分：何ｃｍごと：");
+			System.out.print("増分何ｃｍごと：");
 			//計算できるように処理
 			differenceValue = standardInput.nextInt();
 		//負の値が入力されると再度入力
-		}while(differenceValue<0);
+		}while(differenceValue<=0);
 		//項目を表示
 		System.out.println("身長"+"      "+"標準体重");
 		//開始値から終了値を増分だけ増えていくように設定
