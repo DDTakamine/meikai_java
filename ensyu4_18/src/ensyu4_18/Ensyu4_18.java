ac04f7a9f00fdd2541eaee9d4fc6532ac84aa029
ddt-akamine <a@abc.com>
2023-04-12 13:56:32 +0900

    aaa

--- ensyu4_18/src/ensyu4_18/Ensyu4_18.java
+++ ensyu4_18/src/ensyu4_18/Ensyu4_18.java
@@ -17,11 +17,18 @@ public class Ensyu4_18 {
 	public static void main(String[] args){
 		//標準入力するための準備
 		Scanner standardInput = new Scanner(System.in);
-		//入力を促す
-		System.out.println("ｎの値：");
-		//計算できるように処理
-		int maxValue = standardInput.nextInt();
-		//１乗、2乗、3乗、・・・と計算していく
+		//入力された値を保存
+		//1807298の２乗は-2144051452となる
+		//1807298を境目に負となる
+		int maxValue=0;
+		do{
+			//入力を促す
+			System.out.println("ｎの値：");
+			//計算できるように処理
+			maxValue = standardInput.nextInt();
+		//１より小さい値が入力された場合
+		}while(maxValue<1);
+		//１の２乗、２の２乗、３の２乗、・・・と計算していく
 		for(int i=1; i<maxValue+1; i++) {
 			//結果の表示
 			System.out.println(i + "の２乗は" + i*i);
