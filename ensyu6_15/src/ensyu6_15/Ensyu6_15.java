25f49d2b29a0b0eb9dae314cd8796cfcb1903572
ddt-akamine <a@abc.com>
2023-04-17 13:48:26 +0900

    asdfa

--- ensyu6_15/src/ensyu6_15/Ensyu6_15.java
+++ ensyu6_15/src/ensyu6_15/Ensyu6_15.java
@@ -4,11 +4,11 @@ import java.util.Random;
 //標準入力準備
 import java.util.Scanner;
 /*
-* クラス名:ensyu6_15
-* 概要  :演習6-15
-* 作成者:R.Akamine
-* 作成日:2023/04/17
-*/
+ * クラス名:ensyu6_15
+ * 概要  :演習6-15
+ * 作成者:R.Akamine
+ * 作成日:2023/04/17
+ */
 public class Ensyu6_15 {
 	/*
 	 * 関数名：main
@@ -26,10 +26,10 @@ public class Ensyu6_15 {
 		final int RANDOM_RANGE = 7;
 		//正解を配列にまとめる
 		String[] answerDay = {"sunday"  , "monday" , "tuesday"  ,"wednesday" ,
-								"thursday", "friday" , "saturday"   };
+				"thursday", "friday" , "saturday"   };
 		//問題を出す際に、表示する文字列
 		String[] questionDay = { "日曜日","月曜日","火曜日","水曜日",
-							  "木曜日","金曜日","土曜日"};
+				"木曜日","金曜日","土曜日"};
 		//再度、出題するかどうか聞く際の変数
 		int loopControl     = 0 ;
 		//何曜日を出題するか乱数を格納
@@ -46,7 +46,7 @@ public class Ensyu6_15 {
 			do {
 				//1~RANDOM_RANGEまでで何月の質問をするのか決める
 				questionNumber = randomValue.nextInt(RANDOM_RANGE) + 1 ;
-			//前の問題と同じ問題になっていたら再度問題を選ぶ
+				//前の問題と同じ問題になっていたら再度問題を選ぶ
 			}while(questionNumber == notSameQuestion);
 			//今の問題番号をメモ
 			notSameQuestion = questionNumber;
@@ -58,18 +58,18 @@ public class Ensyu6_15 {
 			if(answerDay[questionNumber-1].equals(inputString)  ==  true) {
 				//正解表示
 				System.out.println("正解です!!");
-			//不正解ならば、
+				//不正解ならば、
 			}else {
 				//繰り返し
 				do {
-				//不正解と表示
-				System.out.println("不正解です。再入力します。");
-				//入力を受け取る
-				inputString = standardInput.next();
-			//不正解の間、
-			}while(answerDay[questionNumber-1].equals(inputString)  !=  true);
-				//不正解と表示
-				System.out.println("不正解です。");
+					//不正解と表示
+					System.out.println("不正解です。再入力します。");
+					//入力を受け取る
+					inputString = standardInput.next();
+					//不正解の間、
+				}while(answerDay[questionNumber-1].equals(inputString)  !=  true);
+				//正解表示
+				System.out.println("正解です!!");
 			}
 			//もう一度問題を出すか？
 			System.out.print("もう一度？  1...Yes  それ以外...No  :  ");
@@ -77,7 +77,7 @@ public class Ensyu6_15 {
 			loopControl = standardInput.nextInt();
 			//整える改行
 			System.out.println("\n");
-		//1の間、質問を出し続ける
+			//1の間、質問を出し続ける
 		}while(loopControl == 1);
 
 	}
