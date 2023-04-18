e9255978df91b18ac08d05ec4c417f0671563e9a
ddt-akamine <a@abc.com>
2023-04-18 17:41:06 +0900

    aaa

--- ensyu7_10/src/ensyu7_10/Ensyu7_10.java
+++ ensyu7_10/src/ensyu7_10/Ensyu7_10.java
@@ -10,17 +10,19 @@ import java.util.Scanner;
  * 作成日:2023/04/18
  */
 public class Ensyu7_10 {
+	//標準入力の準備
+	static Scanner standardInput = new Scanner(System.in);
+	//乱数発生の準備
+	static Random randomValue = new Random();
+	//問題数を定義
+	static final int QUESTION_NUMBER = 4;
 	/*
-	 * 関数名：main
-	 * 概要  :List7-11を改良し、４つの問題がランダムに出題するようにする。
+	 * 関数名：confirmRetry
+	 * 概要  :もう一度、問題を出すかを聞く
 	 * 引数  :なし
-	 * 戻り値:なし
+	 * 戻り値:boolean型　もう一度出題：true
 	 * 作成日:2023.04.18
 	 */
-	static Scanner standardInput = new Scanner(System.in);
-	static Random randomValue = new Random();
-	static final int QUESTION_NUMBER = 4;
-	
 	static boolean confirmRetry() {
 		int countValue = 0 ;
 		do {
@@ -30,15 +32,20 @@ public class Ensyu7_10 {
 		return countValue == 1;
 	}
 	/*
-	 * 関数名：main
-	 * 概要  :List7-11を改良し、４つの問題がランダムに出題するようにする。
-	 * 引数  :なし
-	 * 戻り値:なし
+	 * 関数名：selectQuestionAnswer
+	 * 概要  :問題を選び、それに対応する答えを返す
+	 * 引数  :int型　firstValue　 第一オペランド
+	 * 				 secondValue　第二オペランド
+	 * 				thirdOperand  第三オペランド
+	 * 戻り値:正解の数値
 	 * 作成日:2023.04.18
 	 */
 	static int selectQuestionAnswer(int firstValue, int secondValue,int thirdValue) {
+		//ランダムに問題を選ぶ
 		int randomQuestion = randomValue.nextInt(QUESTION_NUMBER);
+		//正解の数値を保存
 		int correctAnswer=0;
+		//問題番号で場合分け
 		switch(randomQuestion) {
 		//問題１の場合
 		case 0:
@@ -56,7 +63,7 @@ public class Ensyu7_10 {
 			correctAnswer = firstValue + secondValue - thirdValue ;
 			//switch文から抜ける
 			break;
-			//問題３の場合
+		//問題３の場合
 		case 2:
 			//問題を表示する
 			System.out.print(firstValue+"-"+secondValue+"+"+thirdValue+"=");
@@ -73,6 +80,7 @@ public class Ensyu7_10 {
 			//switch文から抜ける
 			break;
 		}
+		//正解の数値を返す
 		return correctAnswer;
 	}
 	
@@ -85,7 +93,7 @@ public class Ensyu7_10 {
 	 */
 	public static void main(String[] args){
 		Random randomValue = new Random();
-		
+		//プログラム説明
 		System.out.println("暗算力トレーニング!!");
 		//繰り返し
 		do {
@@ -97,17 +105,23 @@ public class Ensyu7_10 {
 			int thirdOperand = randomValue.nextInt(900)+100;
 			//正解を保存
 			int correctAnswer=0;
-			//
+			//入力した値を保存する変数
 			int inputAnswer  =0;
+			//返ってきた数値を保存
 			correctAnswer = selectQuestionAnswer(firstOperand,secondOperand,thirdOperand);
+			//正解するまで繰り返す
 			while(true) {
-				
+				//入力した値を保存
 				inputAnswer = standardInput.nextInt();
+				//もし、正解したら
 				if(inputAnswer == correctAnswer) {
+					//while文から抜ける
 					break;
 				}
+				//不正解を知らせる
 				System.out.println("違いますよ！！");
 			}
+		//ユーザーにもう一度問題を出してほしいか聞く
 		}while(confirmRetry());
 	}
 }
