aad015acc961feef0071a969b1481ece1df78684
ddt-akamine <a@abc.com>
2023-04-18 11:58:02 +0900

    ensyu7_5

---
+++ ensyu7_5/src/ensyu7_5/Ensyu7_5.java
@@ -0,0 +1,31 @@
+package ensyu7_5;
+/*
+ * クラス名:ensyu7_5
+ * 概要  :演習7-5
+ * 作成者:R.Akamine
+ * 作成日:2023/04/18
+ */
+public class Ensyu7_5 {
+	/*
+	 * 関数名：hello
+	 * 概要  :こんにちは。と表示する
+	 * 引数  :なし
+	 * 戻り値:なし
+	 * 作成日:2023.04.18
+	 */
+	public static void hello(){
+		//こんにちは。と表示
+		System.out.println("こんにちは。");
+	}
+	/*
+	 * 関数名：main
+	 * 概要  :helloメソッドを使うためのメイン
+	 * 引数  :なし
+	 * 戻り値:なし
+	 * 作成日:2023.04.18
+	 */
+	public static void main(String[] args){
+		//helloメソッドを表示
+		hello();
+	}
+}
