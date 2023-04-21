feb688c9f45ec8f2ae72b8941c549bf0bbe12f2e
ddt-akamine <a@abc.com>
2023-04-21 15:38:12 +0900

    修正：bool型変数の初期化

--- ensyu7_28/src/ensyu7_28/Ensyu7_28.java
+++ ensyu7_28/src/ensyu7_28/Ensyu7_28.java
@@ -16,14 +16,14 @@ public class Ensyu7_28 {
 	
 	/*関数名・引数名は教本に従う
 	 * 関数名 :addMatrix
-	 * 概要   :３つの２次元配列を受け取り、行数・列数が等しいかを判定
-	 * 			等しい場合は和を求めた後、trueを返す。等しくない場合は、falseを返す。
+	 * 概要   :行数・列数が等しいとした場合、行列の和を求める
 	 * 引数   :int[][] x  数値が格納されている行列
 	 * 		　 int[][] y　数値が格納されている行列
 	 * 返り値 :int型２次元配列
 	 * 作成日:2023.04.21
 	 */
 	static int[][] addMatrix(int[][] x,int[][] y) {
+		//返却用配列を用意
 		int[][] sumMatrix = new int [x.length][x[0].length];
 		//行に関するループ
 		for(int i=0;i<x.length;i++) {
@@ -33,23 +33,23 @@ public class Ensyu7_28 {
 				sumMatrix[i][j] = x[i][j] + y[i][j];
 			}
 		}
-		//trueを返す
+		//行列を返す
 		return sumMatrix;
 	}
 	/*関数名・引数名は教本に従う
 	 * 関数名 :printMatrix
 	 * 概要   :２次元配列を行列のように表示
-	 * 引数   :int型　２次元配列　m
+	 * 引数   :int型　２次元配列　printedMatrix
 	 * 返り値 :なし
 	 * 作成日:2023.04.21
 	 */
-	static void printMatrix(int[][] m) {
+	static void printMatrix(int[][] printedMatrix) {
 		//行に関するループ
-		for(int i=0; i<m.length; i++) {
+		for(int i=0; i<printedMatrix.length; i++) {
 			//列に関するループ
-			for(int j=0; j<m[i].length; j++) {
+			for(int j=0; j<printedMatrix[i].length; j++) {
 				//要素を表示して、スペースを表示
-				System.out.print(m[i][j]+"\t");
+				System.out.print(printedMatrix[i][j]+"\t");
 			}
 			//１つ行の表示が終わったので、改行
 			System.out.println();
@@ -63,15 +63,12 @@ public class Ensyu7_28 {
 	 * 作成日:2023.04.21
 	 */
 	public static void main(String[] args) {
-		//デバック用　,{4, 5, 6}
 		//配列を初期化
 		int[][] firstMatrix = { {9, 3, 3} , {4, 5, 6} };
 		//配列を初期化
 		int[][] secondMatrix = { {6, 3, 4} , {5, 1, 2} };
 		//配列を初期化
 		int[][] resultMatrix = new int[2][3];
-		//addMatrixから返ってきた真偽値を格納
-		boolean trueOrFalse;
 		//配列要素の和をもとめる
 		resultMatrix = addMatrix(firstMatrix,secondMatrix);
 		//行列aの表示を知らせる
@@ -82,7 +79,6 @@ public class Ensyu7_28 {
 		System.out.println("行列b");
 		//行列b表示
 		printMatrix(secondMatrix);
-
 		//行列cの表示を知らせる
 		System.out.println("行列c");
 		//行列cの表示
